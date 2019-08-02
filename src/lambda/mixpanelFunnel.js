dd7423fb43de739dd2ca2d648550a741ef2f86b8
Craig Wattrus <craig.wattrus@getbraintree.com>
2019-08-02 15:02:28 -0500

    Adding some auth

--- src/lambda/mixpanelFunnel.js
+++ src/lambda/mixpanelFunnel.js
@@ -3,7 +3,10 @@ import fetch from "node-fetch";
 const API_ENDPOINT = 'https://mixpanel.com/api/2.0/funnels/?funnel_id=6321819&from_date=2019-07-01&to_date=2019-07-31&length=1&interval=31&on=(properties["$current_url"])';
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: {
+  const params = querystring.parse(event.body);
+  const auth = params.auth || "";
+  if(auth==="b6063452-f65a-44fa-bcb0-1d223337a84f") {
+    return fetch(API_ENDPOINT, { headers: {
       "Accept": "application/json",
       "Authorization": "Basic Y2U1NjBmNjYyODY5MDUwNjA1YTI2ZTQ0MWU5OTBmZTQ6"
     }
@@ -14,4 +17,8 @@ exports.handler = async (event, context) => {
       body: data.data
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
+  }
+  else {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
 };
