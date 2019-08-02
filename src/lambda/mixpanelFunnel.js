73db5024d54506484379035e3ee282df6190b01a
Craig Wattrus <craig.wattrus@getbraintree.com>
2019-08-02 15:26:05 -0500

    trying another query param thing

--- src/lambda/mixpanelFunnel.js
+++ src/lambda/mixpanelFunnel.js
@@ -1,16 +1,14 @@
 import fetch from "node-fetch";
-import querystring from "querystring";
 
 const API_ENDPOINT = 'https://mixpanel.com/api/2.0/funnels/?funnel_id=6321819&from_date=2019-07-01&to_date=2019-07-31&length=1&interval=31&on=(properties["$current_url"])';
 
 exports.handler = async (event, context) => {
-  const params = querystring.parse(event.body);
-  const auth = params.auth || "";
+  const auth = event.queryStringParameters.auth
   console.log(params);
   console.log(auth);
   return fetch(API_ENDPOINT, { headers: {
       "Accept": "application/json",
-      "Authorization": "Basic " + "Y2U1NjBmNjYyODY5MDUwNjA1YTI2ZTQ0MWU5OTBmZTQ6"
+      "Authorization": "Basic " + auth
     }
   })
     .then(response => response.json())
