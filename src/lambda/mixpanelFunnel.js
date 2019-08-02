153f7c0e8f7fffd8ccfbf1de768725079791bd65
Craig Wattrus <craig.wattrus@getbraintree.com>
2019-08-02 14:54:44 -0500

    test

--- src/lambda/mixpanelFunnel.js
+++ src/lambda/mixpanelFunnel.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-const API_ENDPOINT = "https://mixpanel.com/api/2.0/funnels/?funnel_id=6321819&from_date=2019-07-01&to_date=2019-07-31&length=1&interval=31&on=(properties['$current_url'])";
+const API_ENDPOINT = 'https://mixpanel.com/api/2.0/funnels/?funnel_id=6321819&from_date=2019-07-01&to_date=2019-07-31&length=1&interval=31&on=(properties["$current_url"])';
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT, { headers: {
