0f6365f02507fa0e8f187eb324d581f655b7b42a
Craig Wattrus <craig.wattrus@getbraintree.com>
2019-08-02 15:08:08 -0500

    Test

--- src/lambda/mixpanelFunnel.js
+++ src/lambda/mixpanelFunnel.js
@@ -15,7 +15,7 @@ exports.handler = async (event, context) => {
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: data.data
+      body: data
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
